91707930fae4b0c99f5cdc6595da0e584c9f89f8
Gordon Brandt <gbrandt@synchronet.net>
2019-05-24 15:52:41 -0400

    started work on high scores/high_scores

--- high-scores/high_scores.py
+++ high-scores/high_scores.py
@@ -1,5 +1,20 @@
+'''
+Use list to hold scores
+list.pop() - removes and returns last item in list - note that it removes! templist = list does not copy, only pointer!
+
+
+
+
+
+
+
+'''
+
 def latest(scores):
-    pass
+    tempscore = scores  # doesn't copy, still removes last added
+    last_added = tempscore.pop() # get the last added score
+    return last_added
+
 
 
 def personal_best(scores):
