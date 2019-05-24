c9fee25db7ea768e22aa86977f62ed8a66efa686
Gordon Brandt <gbrandt@synchronet.net>
2019-05-24 10:35:30 -0400

    Finshed Leap, starting Pangram and Robot-name

--- leap/leap.py
+++ leap/leap.py
@@ -1,2 +1,23 @@
+# PseudoCode
+# function is_leap_year(year)
+#   if year / 4
+#        true - and if year / 100
+#            true - then check year / 400
+#                if true return true
+#                if false return false
+#         false - return true
+#    if year not / 4 then false
+
+
 def is_leap_year(year):
-    pass
+    if year % 4 == 0:
+        if year % 100 == 0:
+            if year % 400 == 0:
+                return True
+            else:
+                return False    
+        else:
+            return True    
+    else:
+        return False
+    
