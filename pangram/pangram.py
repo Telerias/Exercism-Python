532a6a414cac88df09cd45d810d957d190434720
Gordon Brandt <gbrandt@synchronet.net>
2019-05-24 12:41:09 -0400

    finished the Pangram exercise

--- pangram/pangram.py
+++ pangram/pangram.py
@@ -1,2 +1,27 @@
+'''
+Use String methods
+str.lower() to convert to lowercase
+
+After solving, reviewed solutions and Tahnan's solution using sets is more elegant:
+---
+from string import ascii_lowercase
+
+ALPHABET = set(ascii_lowercase)
+
+def is_pangram(string):
+    return ALPHABET.issubset(string.lower())
+---
+
+So, on the right path with strings and sets, but more to learn
+
+'''
+from string import ascii_lowercase as asc_lower
+
 def is_pangram(sentence):
-    pass
+    sentence = sentence.lower() # convert to lower case
+    return set(asc_lower) - set(sentence) == set([])
+
+
+
+
+
