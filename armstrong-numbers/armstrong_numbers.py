a0965beff3701df2d81b0e1943c5182865d2fc94
Gordon Brandt <gbrandt@synchronet.net>
2019-05-29 14:38:29 -0400

    completed the armstrong numbers project

--- armstrong-numbers/armstrong_numbers.py
+++ armstrong-numbers/armstrong_numbers.py
@@ -1,2 +1,14 @@
 def is_armstrong_number(number):
-    pass
+    length = len(str(number))
+
+    sum = 0
+    checknum = number
+
+    while(checknum != 0):
+        sum = sum + ((checknum % 10) ** length)
+        checknum = checknum // 10
+    if sum == number:
+        return True
+    else:
+        return False
+
