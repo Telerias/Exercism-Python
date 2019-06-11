dac500d9dfdc221f0d53f12369088f574d0dbde8
Gordon Brandt <gbrandt@synchronet.net>
2019-06-11 17:03:07 -0400

    intial test for robot_name, fails and submitted for feedback

--- robot-name/robot_name.py
+++ robot-name/robot_name.py
@@ -1,3 +1,27 @@
+# Import String and Random methods
+import string , random
+
+# Define letters and numbers choices
+letters = string.ascii_uppercase
+numbers = string.digits
+    
+usedNames = []
+
 class Robot(object):
+
+
     def __init__(self):
-        pass
+        self.name = self.reset()
+        usedNames.append(self.name)
+
+    def reset(self):
+        # define using random.choices 'Return list with replacement' so possible to get BB or 111
+        name_chars = random.choices(letters) + random.choices(letters)
+        name_numbers = random.choices(numbers) + random.choices(numbers) + random.choices(numbers)            
+        
+        nameChoice = name_chars + name_numbers
+
+        if nameChoice in usedNames :
+            self.reset()
+        
+        return nameChoice
