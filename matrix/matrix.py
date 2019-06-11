3f928b40cda54fb5b805c0c3f153ebc12037fbef
Gordon Brandt <gbrandt@synchronet.net>
2019-06-11 15:33:35 -0400

    matrix problem submission

--- matrix/matrix.py
+++ matrix/matrix.py
@@ -1,9 +1,17 @@
+# \n = new line
+# ' ' = split between numbers
+# str.split("\n") = split into rows
+# int(NumberAsString) = convert string to number
+
 class Matrix(object):
     def __init__(self, matrix_string):
-        pass
+       self.matrix = [
+           [int(element) for element in row.split(' ')] 
+           for row in matrix_string.split('\n')
+               ]
 
     def row(self, index):
-        pass
+        return self.matrix[index-1]
 
     def column(self, index):
-        pass
+        return [row[index-1] for row in self.matrix]
